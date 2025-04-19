efa3175b04b317f77dc92415576109276611588d
Ravi Ranjan <ravi.ranjan@pw.live>
2025-04-19 08:56:29 +0530

    Initial Commit

---
+++ Brooklyn Health/Brooklyn_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brooklyn_HealthApp.swift
+//  Brooklyn Health
+//
+//  Created by Ravi Ranjan on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Brooklyn_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
